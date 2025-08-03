e2f3ce859e474ebea86a75af4d0ac6b51e3dda16
V.V <sajini.v-v@capgemini.com>
2025-08-03 13:51:04 +0530

    test

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -7,7 +7,7 @@ const user = useUser();
   return (
     <div className="card text-center mb-4">
       <img src={user.image} className="card-img-top rounded-circle mx-auto mt-3" style={{ width: '100px', height: '100px' }} alt="User" />
-      <div className="card-body">
+      <div className="card-body"> 
         <h5 className="card-title" role='user-name'>{user.name}</h5>
         <p className="card-text" role='user-email'>{user.email}</p>
         <p className="card-text" role='user-url'>{user.url}</p>
